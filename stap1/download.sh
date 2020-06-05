5621fd29fbd5222f52af77d6d5c3fc3a9880e0ce
MDSKUL <65949210+MDSKUL@users.noreply.github.com>
2020-06-05 11:37:17 +0200

    Update download.sh

--- stap1/download.sh
+++ stap1/download.sh
@@ -2,6 +2,6 @@
 
 while read id; do
   echo "processing: $id"
-  youtube-dl -f --match-filter 'duration < 900' "http://www.youtube.com/watch?v=$id" --add-header "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36" --output "VID_%(id)s.%(ext)s"
+  youtube-dl "http://www.youtube.com/watch?v=$id" --output "%(id)s.%(ext)s"
   sleep 2
 done < ids.txt

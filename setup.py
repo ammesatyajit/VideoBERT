9da09a2c250ae702a713b70c9db57f2292e6077c
ammesatyajit <ammesatyajit@gmail.com>
2020-08-30 14:48:29 -0700

    setup.py file created

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="VideoBERT", # Replace with your own username
+    version="0.0.1",
+    author="Satyajit Kumar",
+    author_email="ammesatyajit@gmail.com",
+    description="Reproducing the results of VideoBERT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ammesatyajit/VideoBERT",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "unlicensed",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

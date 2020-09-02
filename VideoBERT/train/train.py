98300f62a5c00f33ec0a0c08bff44ba9f4527a10
ammesatyajit <ammesatyajit@gmail.com>
2020-09-02 15:25:21 -0700

    made a ColabArgs.py to replace argparse for training

--- VideoBERT/train/train.py
+++ VideoBERT/train/train.py
@@ -150,15 +150,19 @@ def mask_tokens(inputs: torch.Tensor, tokenizer: PreTrainedTokenizer, args) -> T
 
 def train(args, train_dataset, model: PreTrainedModel, tokenizer: PreTrainedTokenizer) -> Tuple[int, float]:
     """ Train the model """
+    # will graph summary of training and eval at the end of each epoch (i think)
     tb_writer = SummaryWriter()
 
+    # Calculates the batch size for training given number of gpus and batch size for gpus
     args.train_batch_size = args.per_gpu_train_batch_size * max(1, args.n_gpu)
 
+    # pads each sentence
     def pad_examples(examples, padding_value=tokenizer.pad_token_id):
         if tokenizer._pad_token is None:
             return pad_sequence(examples, batch_first=True)
         return pad_sequence(examples, batch_first=True, padding_value=padding_value)
 
+    # returns data that is converted to tensor and padded, as well as ids and attention mask
     def collate(examples):
         text_examples = [None] * len(examples)
         text_labels = [None] * len(examples)
@@ -210,11 +214,13 @@ def train(args, train_dataset, model: PreTrainedModel, tokenizer: PreTrainedToke
                pad_examples(joint_type_ids, padding_value=0), \
                joint_attention_mask
 
+    # initializes dataloader
     train_sampler = RandomSampler(train_dataset)
     train_dataloader = DataLoader(
         train_dataset, sampler=train_sampler, batch_size=args.train_batch_size, collate_fn=collate
     )
 
+    # calculates number of epochs based on number of steps to take in training
     if args.max_steps > 0:
         t_total = args.max_steps
         args.num_train_epochs = args.max_steps // (len(train_dataloader) // args.gradient_accumulation_steps) + 1
@@ -435,100 +441,103 @@ def train(args, train_dataset, model: PreTrainedModel, tokenizer: PreTrainedToke
     return global_step, tr_loss / global_step
 
 
-def main():
-    parser = argparse.ArgumentParser()
+def main(colab_args=None):
+    if colab_args:
+        args = colab_args
+    else:
+        parser = argparse.ArgumentParser()
 
-    parser.add_argument(
-        "--output_dir",
-        type=str,
-        required=True,
-        help="The output directory where the model predictions and checkpoints will be written.",
-    )
-    parser.add_argument(
-        "--model_type", type=str, required=True, help="The model architecture to be trained or fine-tuned.",
-    )
-    parser.add_argument(
-        "--should_continue", action="store_true", help="Whether to continue from latest checkpoint in output_dir"
-    )
-    parser.add_argument(
-        "--model_name_or_path",
-        default=None,
-        type=str,
-        help="The model checkpoint for weights initialization. Leave None if you want to train a model from scratch.",
-    )
-    parser.add_argument(
-        "--mlm", action="store_true", help="Train with masked-language modeling loss instead of language modeling."
-    )
-    parser.add_argument(
-        "--mlm_probability", type=float, default=0.15, help="Ratio of tokens to mask for masked language modeling loss"
-    )
-    parser.add_argument(
-        "--config_name",
-        default=None,
-        type=str,
-        help="Optional pretrained config name or path if not the same as model_name_or_path. If both are None, initialize a new config.",
-    )
-    parser.add_argument(
-        "--tokenizer_name",
-        default=None,
-        type=str,
-        help="Optional pretrained tokenizer name or path if not the same as model_name_or_path. If both are None, initialize a new tokenizer.",
-    )
-    parser.add_argument(
-        "--cache_dir",
-        default=None,
-        type=str,
-        help="Optional directory to store the pre-trained models downloaded from s3 (instead of the default one)",
-    )
-    parser.add_argument(
-        "--block_size",
-        default=-1,
-        type=int,
-        help="Optional input sequence length after tokenization."
-        "The training dataset will be truncated in block of this size for training."
-        "Default to the model max input length for single sentence inputs (take into account special tokens).",
-    )
-    parser.add_argument("--do_train", action="store_true", help="Whether to run training.")
-    parser.add_argument("--per_gpu_train_batch_size", default=4, type=int, help="Batch size per GPU/CPU for training.")
-    parser.add_argument(
-        "--gradient_accumulation_steps",
-        type=int,
-        default=1,
-        help="Number of updates steps to accumulate before performing a backward/update pass.",
-    )
-    parser.add_argument("--learning_rate", default=1e-5, type=float, help="The initial learning rate for Adam.")
-    parser.add_argument("--weight_decay", default=0.0, type=float, help="Weight decay if we apply some.")
-    parser.add_argument("--adam_epsilon", default=1e-8, type=float, help="Epsilon for Adam optimizer.")
-    parser.add_argument("--max_grad_norm", default=1.0, type=float, help="Max gradient norm.")
-    parser.add_argument(
-        "--num_train_epochs", default=1.0, type=float, help="Total number of training epochs to perform."
-    )
-    parser.add_argument(
-        "--max_steps",
-        default=-1,
-        type=int,
-        help="If > 0: set total number of training steps to perform. Override num_train_epochs.",
-    )
-    parser.add_argument("--log_dir", default=".", type=str, help="Directory to store the logs.")
-    parser.add_argument("--warmup_steps", default=0, type=int, help="Linear warmup over warmup_steps.")
-    parser.add_argument("--local_rank", type=int, default=-1, help="For distributed training: local_rank")
-    parser.add_argument("--logging_steps", type=int, default=500, help="Log every X updates steps.")
-    parser.add_argument("--save_steps", type=int, default=500, help="Save checkpoint every X updates steps.")
-    parser.add_argument(
-        "--save_total_limit",
-        type=int,
-        default=None,
-        help="Limit the total amount of checkpoints, delete the older checkpoints in the output_dir, does not delete by default",
-    )
-    parser.add_argument("--no_cuda", action="store_true", help="Avoid using CUDA when available")
-    parser.add_argument(
-        "--overwrite_output_dir", action="store_true", help="Overwrite the content of the output directory"
-    )
-    parser.add_argument(
-        "--overwrite_cache", action="store_true", help="Overwrite the cached training and evaluation sets"
-    )
-    parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
-    args = parser.parse_args()
+        parser.add_argument(
+            "--output_dir",
+            type=str,
+            required=True,
+            help="The output directory where the model predictions and checkpoints will be written.",
+        )
+        parser.add_argument(
+            "--model_type", type=str, required=True, help="The model architecture to be trained or fine-tuned.",
+        )
+        parser.add_argument(
+            "--should_continue", action="store_true", help="Whether to continue from latest checkpoint in output_dir"
+        )
+        parser.add_argument(
+            "--model_name_or_path",
+            default=None,
+            type=str,
+            help="The model checkpoint for weights initialization. Leave None if you want to train a model from scratch.",
+        )
+        parser.add_argument(
+            "--mlm", action="store_true", help="Train with masked-language modeling loss instead of language modeling."
+        )
+        parser.add_argument(
+            "--mlm_probability", type=float, default=0.15, help="Ratio of tokens to mask for masked language modeling loss"
+        )
+        parser.add_argument(
+            "--config_name",
+            default=None,
+            type=str,
+            help="Optional pretrained config name or path if not the same as model_name_or_path. If both are None, initialize a new config.",
+        )
+        parser.add_argument(
+            "--tokenizer_name",
+            default=None,
+            type=str,
+            help="Optional pretrained tokenizer name or path if not the same as model_name_or_path. If both are None, initialize a new tokenizer.",
+        )
+        parser.add_argument(
+            "--cache_dir",
+            default=None,
+            type=str,
+            help="Optional directory to store the pre-trained models downloaded from s3 (instead of the default one)",
+        )
+        parser.add_argument(
+            "--block_size",
+            default=-1,
+            type=int,
+            help="Optional input sequence length after tokenization."
+            "The training dataset will be truncated in block of this size for training."
+            "Default to the model max input length for single sentence inputs (take into account special tokens).",
+        )
+        parser.add_argument("--do_train", action="store_true", help="Whether to run training.")
+        parser.add_argument("--per_gpu_train_batch_size", default=4, type=int, help="Batch size per GPU/CPU for training.")
+        parser.add_argument(
+            "--gradient_accumulation_steps",
+            type=int,
+            default=1,
+            help="Number of updates steps to accumulate before performing a backward/update pass.",
+        )
+        parser.add_argument("--learning_rate", default=1e-5, type=float, help="The initial learning rate for Adam.")
+        parser.add_argument("--weight_decay", default=0.0, type=float, help="Weight decay if we apply some.")
+        parser.add_argument("--adam_epsilon", default=1e-8, type=float, help="Epsilon for Adam optimizer.")
+        parser.add_argument("--max_grad_norm", default=1.0, type=float, help="Max gradient norm.")
+        parser.add_argument(
+            "--num_train_epochs", default=1.0, type=float, help="Total number of training epochs to perform."
+        )
+        parser.add_argument(
+            "--max_steps",
+            default=-1,
+            type=int,
+            help="If > 0: set total number of training steps to perform. Override num_train_epochs.",
+        )
+        parser.add_argument("--log_dir", default=".", type=str, help="Directory to store the logs.")
+        parser.add_argument("--warmup_steps", default=0, type=int, help="Linear warmup over warmup_steps.")
+        parser.add_argument("--local_rank", type=int, default=-1, help="For distributed training: local_rank")
+        parser.add_argument("--logging_steps", type=int, default=500, help="Log every X updates steps.")
+        parser.add_argument("--save_steps", type=int, default=500, help="Save checkpoint every X updates steps.")
+        parser.add_argument(
+            "--save_total_limit",
+            type=int,
+            default=None,
+            help="Limit the total amount of checkpoints, delete the older checkpoints in the output_dir, does not delete by default",
+        )
+        parser.add_argument("--no_cuda", action="store_true", help="Avoid using CUDA when available")
+        parser.add_argument(
+            "--overwrite_output_dir", action="store_true", help="Overwrite the content of the output directory"
+        )
+        parser.add_argument(
+            "--overwrite_cache", action="store_true", help="Overwrite the cached training and evaluation sets"
+        )
+        parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
+        args = parser.parse_args()
 
     if args.model_type in ["bert", "roberta", "distilbert", "camembert"] and not args.mlm:
         raise ValueError(

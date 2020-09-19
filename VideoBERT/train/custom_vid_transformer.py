c6100d30f8ac8bc67f9e77d8552babbf8496bec6
ammesatyajit <ammesatyajit@gmail.com>
2020-09-19 15:41:35 -0700

    adding components to the transformer

--- VideoBERT/train/custom_vid_transformer.py
+++ VideoBERT/train/custom_vid_transformer.py
@@ -154,7 +154,7 @@ class VideoTransformer(nn.Module):
     def forward(self, seq):
         # seq - [batch_size, seq_len]
         pos = torch.arange(0, seq.shape[1]).unsqueeze(0).repeat(seq.shape[0], 1).to(self.args.device)
-        print(any(torch.isnan(self.tok_embed(seq)).view(-1)))
+        print(any(torch.isnan(self.pos_encoding(pos)).view(-1)))
         seq = (self.dropout((self.tok_embed(seq) * self.scale) + self.pos_encoding(pos))).transpose(0, 1)
         out = self.transformer(seq, seq).transpose(0, 1)
         print(seq, '\n', out)

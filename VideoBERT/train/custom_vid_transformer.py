bb6e0e628aca6bb52f8237866f457361d5d6756f
ammesatyajit <ammesatyajit@gmail.com>
2020-10-14 08:39:52 -0700

    adding num_layers variable to transformer

--- VideoBERT/train/custom_vid_transformer.py
+++ VideoBERT/train/custom_vid_transformer.py
@@ -153,8 +153,14 @@ class VideoTransformer(nn.Module):
         assert contains_nan(self.scale).item() is False
 
         self.fc_out = nn.Linear(self.config.hidden_size, self.config.vocab_size)
+        print(self.config.num_hidden_layers/2)
 
-        self.transformer = nn.Transformer(d_model=self.config.hidden_size, nhead=self.config.num_attention_heads, num_encoder_layers=self.config.num_hidden_layers/2, num_decoder_layers=self.config.num_hidden_layers/2, activation=self.config.hidden_act, dropout=0.1)
+        self.transformer = nn.Transformer(d_model=self.config.hidden_size,
+                                          nhead=self.config.num_attention_heads,
+                                          num_encoder_layers=self.config.num_hidden_layers/2,
+                                          num_decoder_layers=self.config.num_hidden_layers/2,
+                                          activation=self.config.hidden_act,
+                                          dropout=0.1)
 
     def forward(
         self,

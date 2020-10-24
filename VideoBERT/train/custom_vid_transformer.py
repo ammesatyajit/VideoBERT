dae172a9d04d18852991e50761fa08fca6df9c45
ammesatyajit <ammesatyajit@gmail.com>
2020-10-24 15:18:14 -0700

    bug fixes for train_lang

--- VideoBERT/train/custom_vid_transformer.py
+++ VideoBERT/train/custom_vid_transformer.py
@@ -11,7 +11,7 @@ class VideoTransformer(nn.Module):
         self.args = args
 
         self.tok_embed = nn.Embedding(self.config.vocab_size, self.config.hidden_size)
-        self.pos_encoding = nn.Embedding(315, self.config.hidden_size)
+        self.pos_encoding = nn.Embedding(400, self.config.hidden_size)
         self.tok_type_embed = nn.Embedding(2, self.config.hidden_size)
 
         self.dropout = nn.Dropout(0.1)

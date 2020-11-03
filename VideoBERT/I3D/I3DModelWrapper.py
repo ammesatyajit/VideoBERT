8e5be20c021b58665bc78e0f11d1695681f7acc8
ammesatyajit <ammesatyajit@gmail.com>
2020-11-02 20:35:16 -0800

    changing the feature extraction portion for testing on colab

--- VideoBERT/I3D/I3DModelWrapper.py
+++ VideoBERT/I3D/I3DModelWrapper.py
@@ -18,7 +18,7 @@ class I3DModel:
             self.sess.close()
 
     def load_graph(self, frozen_graph_filename):
-        with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
+        with tf.io.gfile.GFile(frozen_graph_filename, "rb") as f:
             graph_def = tf.GraphDef()
             graph_def.ParseFromString(f.read())
 

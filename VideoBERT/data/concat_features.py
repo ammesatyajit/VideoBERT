1094ed8eb89e7bae9363ebbdd99e70edbab6bdf7
ammesatyajit <ammesatyajit@gmail.com>
2020-11-18 09:59:13 -0800

    functionality improvements for concat_features.py

--- VideoBERT/data/concat_features.py
+++ VideoBERT/data/concat_features.py
@@ -13,24 +13,25 @@ args = parser.parse_args()
 root_features = args.root_feature_path
 save_path = args.features_save_path
 
-features_concat = None
+# features_concat = None
+# for root, dirs, files in tqdm(os.walk(root_features)):
+#     for name in files:
+#         path = os.path.join(root, name)
+#         features = torch.from_numpy(np.load(path)).cuda()
+#         if features_concat is None:
+#             features_concat = features
+#         else:
+#             features_concat = torch.cat((features_concat, features))
+
+features_concat = []
 for root, dirs, files in tqdm(os.walk(root_features)):
     for name in files:
         path = os.path.join(root, name)
         features = torch.from_numpy(np.load(path)).cuda()
-        if features_concat is None:
-            features_concat = features
-        else:
-            features_concat = torch.cat((features_concat, features))
-
-# features_concat = []
-# for root, dirs, files in tqdm(os.walk(root_features)):
-#     for name in files:
-#         path = os.path.join(root, name)
-#         features_concat.append(torch.from_numpy(np.load(path)).cuda())
+        features_concat.append(features)
 
 print("finished reading all features")
-# print(features_concat[0].shape)
-# features_concat = torch.cat(features_concat)
+print(features_concat[0].shape)
+features_concat = torch.cat(features_concat)
 print("final size:", features_concat.shape)
 np.save(save_path, features_concat)

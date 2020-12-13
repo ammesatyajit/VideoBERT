8844d72c8c4a59b823f3c890cf2957c331884397
ammesatyajit <ammesatyajit@gmail.com>
2020-12-12 16:01:41 -0800

    finishing hkmeans code in minibatch_hkmeans.py

--- VideoBERT/I3D/minibatch_hkmeans.py
+++ VideoBERT/I3D/minibatch_hkmeans.py
@@ -7,10 +7,7 @@ from sklearn.cluster import MiniBatchKMeans
 
 def minibatch_kmeans(root, prefix, k, batch_size, epochs):
     """
-    concatenates all vectors starting with prefix and mini-batch kmeans on
-    them with k as input k and the batch size as batch_size. Returns the
-    centroids and the labelled data which is a vector of the closest centroid
-    to each data point
+    docstring
     """
     paths = []
     for root, dirs, files in tqdm(os.walk(root)):
@@ -20,6 +17,7 @@ def minibatch_kmeans(root, prefix, k, batch_size, epochs):
 
     kmeans = MiniBatchKMeans(n_clusters=k, batch_size=batch_size)
     vectors = None
+
     print("starting kmeans")
     for i in range(epochs):
         print("epoch:", i)
@@ -33,12 +31,73 @@ def minibatch_kmeans(root, prefix, k, batch_size, epochs):
                 vectors = vectors[:batch_size, :]
                 kmeans.partial_fit(vectors)
                 vectors = None
+        if vectors is not None:
+            kmeans.partial_fit(vectors)
+
     print("labelling data")
     labelled_data = {}
     for path in tqdm(paths):
         labelled_data[path] = list(kmeans.predict(np.load(path)))
 
     return kmeans.cluster_centers_, labelled_data
+
+
+def save_sorted_vectors(centroids, labelled_data, batch_size, save_dir, save_prefix):
+    k = centroids.shape[0]
+    save_path = os.path.join(save_dir, save_prefix) + '-{}-Id:{}'
+    for i in range(k):
+        sorted_vecs = []
+        counter = 1
+        for key in tqdm(labelled_data):
+            pred_centroids = labelled_data[key]
+            vectors = np.load(key)
+            for j in range(len(pred_centroids)):
+                if pred_centroids[j] == i:
+                    sorted_vecs.append(np.expand_dims(vectors[j], axis=0))
+                    if len(sorted_vecs) == batch_size:
+                        sorted_vecs = np.concatenate(sorted_vecs)
+                        np.save(save_path.format(i, counter), sorted_vecs)
+                        sorted_vecs = []
+                        counter += 1
+
+        if sorted_vecs != []:
+            sorted_vecs = np.concatenate(sorted_vecs)
+            np.save(save_path.format(i, counter), sorted_vecs)
+            sorted_vecs = []
+
+
+def delete_used_files(root, prefix):
+    print("deleting finished files")
+    for root, dirs, files in tqdm(os.walk(root)):
+        for name in files:
+            if name.find(prefix) != -1:
+                os.remove(os.path.join(root, name))
+
+
+def hkmeans(root, prefix, h, k, batch_size, epochs, save_dir, save_prefix, centroid_dir):
+    counter = 1
+
+    def hkmeans_recursive(root, prefix, h, k, batch_size, epochs, save_dir, save_prefix, centroid_dir, cur_h=1):
+        nonlocal counter
+        print("Current H:", cur_h)
+        print(prefix)
+        if cur_h != h:
+            centroids, labelled_data = minibatch_kmeans(root, prefix, k, batch_size, epochs)
+            print("minibatch kmeans done!")
+            save_sorted_vectors(centroids, labelled_data, batch_size, save_dir, save_prefix)
+            save_prefix += '-{}'
+            for i in range(k):
+                hkmeans_recursive(save_dir, save_prefix.format(i) + '-', h, k, batch_size, epochs, save_dir,
+                                  save_prefix.format(i), centroid_dir, cur_h=cur_h + 1)
+                delete_used_files(save_dir, save_prefix.format(i) + '-')
+        else:
+            centroids, labelled_data = minibatch_kmeans(root, prefix, k, batch_size, epochs)
+            print("minibatch kmeans done!")
+            np.save(os.path.join(centroid_dir, 'centroids-{}'.format(counter)), centroids)
+            counter += 1
+
+    hkmeans_recursive(root, prefix, h, k, batch_size, epochs, save_dir, save_prefix, centroid_dir)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -48,16 +107,21 @@ def main():
     parser.add_argument('-p', '--features-prefix', type=str, required=True,
                         help='prefix that contains the desired files to read')
     parser.add_argument('-b', '--batch-size', type=int, default=500,
-                        help='batch_size of kmeans thing')
+                        help='batch_size to use for the minibatch kmeans')
+    parser.add_argument('-s', '--save-dir', type=str, required=True,
+                        help='save directory for hierarchical kmeans vectors')
+    parser.add_argument('-c', '--centroid-dir', type=str, required=True,
+                        help='directory to save the centroids in')
     args = parser.parse_args()
 
     root = args.root_feature_path
     prefix = args.features_prefix
     batch_size = args.batch_size
+    save_dir = args.save_dir
+    centroid_dir = args.centroid_dir
 
-    centroids, labelled_data = minibatch_kmeans(root, prefix, 12, batch_size, 10)
-    print(centroids.shape)
-    print(labelled_data.items[0])
+    hkmeans(root, prefix, 4, 12, batch_size, 15, save_dir, 'vecs', centroid_dir)
+
 
 if __name__ == "__main__":
     main()

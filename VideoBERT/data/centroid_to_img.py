4523d45d4673ddcfde9c1f466d19f27feeec6e53
ammesatyajit <ammesatyajit@gmail.com>
2020-12-14 10:20:00 -0800

    centroid_to_img.py in troch rather than numpy for gpu support

--- VideoBERT/data/centroid_to_img.py
+++ VideoBERT/data/centroid_to_img.py
@@ -1,12 +1,11 @@
 import numpy as np
-import torch
 from tqdm import tqdm
 import os
 import json
 
 saved_features = 'saved_features'
 saved_imgs = 'saved_imgs'
-centroids = torch.from_numpy(np.load('centroids.npy')).cuda()
+centroids = np.load('centroids.npy')
 centroid_map = {}
 
 
@@ -16,21 +15,22 @@ def img_path_from_centroid(feature_dir, centroid, img_dir):
     features_id = None
     features_row = None
 
-    for root, dirs, files in os.walk(feature_dir):
+    for root, dirs, files in tqdm(os.walk(feature_dir)):
         for name in files:
             path = os.path.join(root, name)
-            features = torch.from_numpy(np.load(path)).cuda()
-            centroid_dist = torch.linalg.norm(features - centroid, axis=1)
-            if torch.min(centroid_dist) < min_dist:
-                min_dist = torch.min(centroid_dist)
+            features = np.load(path)
+            centroid_dist = np.linalg.norm(features - centroid, axis=1)
+            if np.min(centroid_dist) < min_dist:
+                min_dist = np.min(centroid_dist)
                 vid_id = path[path.index('/') + 1: path.rindex('/')]
                 features_id = path[path.rindex('-') + 1: path.rindex('.')]
-                features_row = torch.argmin(centroid_dist)
+                features_row = np.argmin(centroid_dist)
 
     return os.path.join(img_dir, vid_id, 'img-{}-{:02}.jpg'.format(features_id, features_row))
 
 
-for i in tqdm(range(centroids.shape[0])):
+for i in range(centroids.shape[0]):
+    print("Centroid:", i)
     centroid_map[i] = img_path_from_centroid(saved_features, centroids[i], saved_imgs)
 
 json.dump(centroid_map, open('centroid_to_img.json', 'wb'), sort_keys=True, indent=4)

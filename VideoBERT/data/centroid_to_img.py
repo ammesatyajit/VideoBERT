07639d479f1aa164d075e578d0bbe80dc2882282
ammesatyajit <ammesatyajit@gmail.com>
2020-12-13 22:52:02 -0800

    finishing hkmeans code in minibatch_hkmeans.py

---
+++ VideoBERT/data/centroid_to_img.py
@@ -0,0 +1,32 @@
+import numpy as np
+from tqdm import tqdm
+import os
+
+saved_features = 'saved_features'
+saved_imgs = 'saved_imgs'
+centroids = np.load('centroids.npy')
+centroid_map = {}
+
+
+def img_path_from_centroid(feature_dir, centroid, img_dir):
+    min_dist = 2 ** 12 - 1
+    vid_id = None
+    features_id = None
+    features_row = None
+
+    for root, dirs, files in tqdm(os.walk(feature_dir)):
+        for name in files:
+            path = os.path.join(root, name)
+            features = np.load(path)
+            centroid_dist = np.linalg.norm(features - centroid, axis=1)
+            if np.min(centroid_dist) < min_dist:
+                min_dist = np.min(centroid_dist)
+                vid_id = path[path.index('/') + 1: path.rindex('/')]
+                features_id = path[path.rindex('-') + 1: path.rindex('.')]
+                features_row = np.argmin(centroid_dist)
+
+    return os.path.join(img_dir, vid_id, 'img-{}-{:02}.jpg'.format(features_id, features_row))
+
+
+# for i in range(centroids.shape[0]):
+#     centroid_map[i] = img_path_from_centroid(saved_features, centroids[1], saved_imgs)

c3001c8923430e1fb82d608e3f4002b780012a00
ammesatyajit <ammesatyajit@gmail.com>
2020-12-13 23:21:48 -0800

    code for creating map of centroid to image and saving it in a json

--- VideoBERT/data/centroid_to_img.py
+++ VideoBERT/data/centroid_to_img.py
@@ -1,6 +1,7 @@
 import numpy as np
 from tqdm import tqdm
 import os
+import json
 
 saved_features = 'saved_features'
 saved_imgs = 'saved_imgs'
@@ -14,7 +15,7 @@ def img_path_from_centroid(feature_dir, centroid, img_dir):
     features_id = None
     features_row = None
 
-    for root, dirs, files in tqdm(os.walk(feature_dir)):
+    for root, dirs, files in os.walk(feature_dir):
         for name in files:
             path = os.path.join(root, name)
             features = np.load(path)
@@ -25,10 +26,10 @@ def img_path_from_centroid(feature_dir, centroid, img_dir):
                 features_id = path[path.rindex('-') + 1: path.rindex('.')]
                 features_row = np.argmin(centroid_dist)
 
-    print(min_dist)
     return os.path.join(img_dir, vid_id, 'img-{}-{:02}.jpg'.format(features_id, features_row))
 
 
-print(img_path_from_centroid(saved_features, centroids[0], saved_imgs))
-# for i in range(centroids.shape[0]):
-#     centroid_map[i] = img_path_from_centroid(saved_features, centroids[1], saved_imgs)
+for i in tqdm(range(centroids.shape[0])):
+    centroid_map[i] = img_path_from_centroid(saved_features, centroids[1], saved_imgs)
+
+json.dump(centroid_map, open('centroid_to_img.json', 'wb'), sort_keys=True, indent=4)

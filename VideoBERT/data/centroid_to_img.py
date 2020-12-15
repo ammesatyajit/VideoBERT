e6cec027ebc3f4a4a2270a4e406ea9dc9fd16856
ammesatyajit <ammesatyajit@gmail.com>
2020-12-14 18:39:51 -0800

    tests with centroid_to_img.py

--- VideoBERT/data/centroid_to_img.py
+++ VideoBERT/data/centroid_to_img.py
@@ -21,28 +21,27 @@ for root, dirs, files in tqdm(os.walk(saved_features)):
         counter += 1
 
 
-def img_path_from_centroid(feature_dir, centroid, img_dir):
+def img_path_from_centroid(features, centroid, img_dir):
     min_dist = 2 ** 12 - 1
     vid_id = None
     features_id = None
     features_row = None
 
-    for root, dirs, files in tqdm(os.walk(feature_dir)):
-        for name in files:
-            path = os.path.join(root, name)
-            features = np.load(path)
-            centroid_dist = np.linalg.norm(features - centroid, axis=1)
-            if np.min(centroid_dist) < min_dist:
-                min_dist = np.min(centroid_dist)
-                vid_id = path[path.index('/') + 1: path.rindex('/')]
-                features_id = path[path.rindex('-') + 1: path.rindex('.')]
-                features_row = np.argmin(centroid_dist)
+    for i in tqdm(range(counter)):
+        centroid_dist = np.linalg.norm(features[i] - centroid, axis=1)
+        if np.min(centroid_dist) < min_dist:
+            path = feature_paths[i]
+            min_dist = np.min(centroid_dist)
+            vid_id = path[path.index('/') + 1: path.rindex('/')]
+            features_id = path[path.rindex('-') + 1: path.rindex('.')]
+            features_row = np.argmin(centroid_dist)
 
     return os.path.join(img_dir, vid_id, 'img-{}-{:02}.jpg'.format(features_id, features_row))
 
 
+img_path_from_centroid(feature_list, centroids[0], saved_imgs)
 # for i in range(centroids.shape[0]):
 #     print("Centroid:", i)
 #     centroid_map[i] = img_path_from_centroid(saved_features, centroids[i], saved_imgs)
-
-json.dump(centroid_map, open('centroid_to_img.json', 'wb'), sort_keys=True, indent=4)
+#
+# json.dump(centroid_map, open('centroid_to_img.json', 'wb'), sort_keys=True, indent=4)

f4b046b7258d30eccb3a8b2b0c65119021d7b0b7
ammesatyajit <ammesatyajit@gmail.com>
2020-12-20 16:41:36 +0530

    tests with centroid_to_img.py

---
+++ VideoBERT/data/label_data.py
@@ -0,0 +1,26 @@
+import numpy as np
+from tqdm import tqdm
+from sklearn.cluster import MiniBatchKMeans
+import os
+import json
+
+features_root = 'saved_features'
+dirs = os.listdir(features_root)
+
+centroids = np.load('centroids.npy')
+kmeans = MiniBatchKMeans()
+kmeans.cluster_centers_ = centroids
+
+save_path = 'labelled_data.json'
+
+data_dict = {}
+
+# for folder in tqdm(dirs):
+folder = dirs[0]
+feature_files = sorted(os.listdir(os.join(features_root, folder)))
+print(feature_files)
+for features in feature_files:
+    data_dict[folder].extend(kmeans.predict(np.load(os.path.join(features_root, folder, features))))
+
+print(data_dict)
+# json.dump(data_dict, open(save_path, 'w'), sort_keys=True, indent=4)

ac34ec39fa7e0e9d35b4f216ec2dc7d22edf08b6
ammesatyajit <ammesatyajit@gmail.com>
2020-12-28 12:52:21 -0800

    bug fixes for compare_quantized_data.py

--- VideoBERT/data/compare_quantized_data.py
+++ VideoBERT/data/compare_quantized_data.py
@@ -17,11 +17,12 @@ def concat_tile(im_list_2d):
 
 centroid_map = json.load(open('centroid_to_img.json', 'r'))
 data = json.load(open('labelled_data.json', 'r'))
+vid_id = 'oI8CMo-pX7I'
 
 real_imgs = [cv2.imread(os.path.join('saved_imgs', vid_id, file)) for file in sorted(os.listdir(os.path.join('saved_imgs', vid_id)))]
 quantized_imgs = [cv2.imread(centroid_map[str(centroid)]) for centroid in data[vid_id]]
 
-real_tile = concat_tile([real_imgs[5*i:5*(i+1)] for i in range(5, 10)])
+real_tile = concat_tile([real_imgs[10*i:10*(i+1)] for i in range(5, 15)])
 quantized_tile = concat_tile([quantized_imgs[10*i:10*(i+1)] for i in range(5, 15)])
 
 cv2.imwrite('real_imgs.jpg', real_tile)

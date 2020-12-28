c047822a972dd45ea6a125ea78e0c6978a5ac7c4
ammesatyajit <ammesatyajit@gmail.com>
2020-12-28 11:11:10 -0800

    bug fixes for compare_quantized_data.py

--- VideoBERT/data/compare_quantized_data.py
+++ VideoBERT/data/compare_quantized_data.py
@@ -12,20 +12,17 @@ centroid_map = json.load(open('centroid_to_img.json', 'r'))
 data = json.load(open('labelled_data.json', 'r'))
 vid_id = 'rl6i8bTPk3Q'
 
-real_imgs = [cv2.imread(file) for file in os.listdir(os.path.join('saved_imgs', vid_id))]
+real_imgs = [cv2.imread(os.path.join('saved_imgs', vid_id, file)) for file in os.listdir(os.path.join('saved_imgs', vid_id))]
 quantized_imgs = [cv2.imread(centroid_map[str(centroid)]) for centroid in data[vid_id]]
 
-# real_tile = concat_tile([real_imgs[20:25],
-#                          real_imgs[25:30],
-#                          real_imgs[30:35],
-#                          real_imgs[35:40]])
-# quantized_tile = concat_tile([quantized_imgs[20:25],
-#                               quantized_imgs[25:30],
-#                               quantized_imgs[30:35],
-#                               quantized_imgs[35:40]])
+real_tile = concat_tile([real_imgs[20:25],
+                         real_imgs[25:30],
+                         real_imgs[30:35],
+                         real_imgs[35:40]])
+quantized_tile = concat_tile([quantized_imgs[20:25],
+                              quantized_imgs[25:30],
+                              quantized_imgs[30:35],
+                              quantized_imgs[35:40]])
 
-real_tile = cv2.hconcat(real_imgs[20:40])
-quantized_tile = cv2.hconcat(quantized_imgs[20:40])
-print(real_tile.shape, quantized_tile.shape)
 cv2.imwrite('real_imgs.jpg', real_tile)
 cv2.imwrite('quantized_imgs.jpg', quantized_tile)

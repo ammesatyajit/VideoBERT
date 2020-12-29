467e6e2e6d09b3ce99e6b51d5bdddfb02526f82c
ammesatyajit <ammesatyajit@gmail.com>
2020-12-28 22:22:44 -0800

    punctuating the text

--- VideoBERT/data/punctuate_text.py
+++ VideoBERT/data/punctuate_text.py
@@ -7,5 +7,6 @@ captions = json.load(open('raw_caption_superclean.json', 'r'))
 vid_ids = os.listdir('saved_imgs')
 
 text = captions[vid_ids]['text']
+print(text)
 punc_text = punc.punctuate(' '.join(text))
 print(punc_text)

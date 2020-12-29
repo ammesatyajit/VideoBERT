0e4045cf633d1835a9e4e00261c0f52c60589c17
ammesatyajit <ammesatyajit@gmail.com>
2020-12-28 22:10:42 -0800

    punctuating the text

--- VideoBERT/data/process_text.py
+++ VideoBERT/data/process_text.py
@@ -5,41 +5,39 @@ from nltk.tokenize import sent_tokenize
 import nltk
 from punctuator import Punctuator
 
-
 punc = Punctuator('model.pcl')
 nltk.download('punkt')
 
 file = 'captions.txt'
 
+
 def parse_line(line):
-  parts = line.split(" ")
-  if len(parts) > 1:
-    id = parts[0]
-    val = " ".join(parts[1:])
-    return id,val
+    parts = line.split(" ")
+    if len(parts) > 1:
+        id = parts[0]
+        val = " ".join(parts[1:])
+        return id, val
 
 
 with open(file, 'r') as fd:
-  while True:
-    line = fd.readline()
-    if not line:
-      break
+    while True:
+        line = fd.readline()
+        if not line:
+            break
 
-    id, valstr = parse_line(line)
-    val = json.loads(valstr)
-    text = val.get("text")
-    text = " ".join(text)
-    text = text.replace("[Music]", " ").replace("\r", " ").replace("\n", " ")
-    text = ' '.join(text.split())
-    sentences = sent_tokenize(text)
+        id, valstr = parse_line(line)
+        val = json.loads(valstr)
+        text = val.get("text")
+        text = " ".join(text)
+        text = text.replace("[Music]", " ").replace("\r", " ").replace("\n", " ")
+        text = ' '.join(text.split())
+        sentences = sent_tokenize(text)
 
-    punc_text = None
-    punc_needed = True
+        punc_text = None
+        punc_needed = True
 
-    if len(sentences) > 2:
-      punc_needed = False
-      punc_text = text
-    else:
-      punc_text = punc.punctuate(text)
-
-    
+        if len(sentences) > 2:
+            punc_needed = False
+            punc_text = text
+        else:
+            punc_text = punc.punctuate(text)
---
+++ VideoBERT/data/punctuate_text.py
@@ -0,0 +1,11 @@
+import json
+import os
+from punctuator import Punctuator
+
+punc = Punctuator('model.pcl')
+captions = json.load(open('raw_caption_superclean.json', 'r'))
+vid_ids = os.listdir('saved_imgs')
+
+text = captions[vid_ids]['text']
+punc_text = punc.punctuate(' '.join(text))
+print(punc_text)

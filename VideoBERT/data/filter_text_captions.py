03d89bca20ef5eaa89343668d693d236ce1576ed
ammesatyajit <ammesatyajit@gmail.com>
2020-12-28 23:30:57 -0800

    filtering caption json

--- VideoBERT/data/filter_text_captions.py
+++ VideoBERT/data/filter_text_captions.py
@@ -1,13 +1,14 @@
 import json
 import os
 import ijson
+from tqdm import tqdm
 
 ids = os.listdir('saved_features')
 filtered_captions = {}
 
 with open('raw_caption_superclean.json', 'r') as input_file:
     captions_json = ijson.kvitems(input_file, '')
-    for vid_id, captions in captions_json:
+    for vid_id, captions in tqdm(captions_json):
         if vid_id in ids:
             filtered_captions[vid_id] = captions
 

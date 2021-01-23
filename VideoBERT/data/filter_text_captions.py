233f485b0fbbbb9a66502f8c0269b01332c075ad
ammesatyajit <ammesatyajit@gmail.com>
2021-01-23 13:04:45 -0800

    more functionality for filter_text_captions.py

--- VideoBERT/data/filter_text_captions.py
+++ VideoBERT/data/filter_text_captions.py
@@ -2,14 +2,25 @@ import simplejson as json
 import os
 import ijson
 from tqdm import tqdm
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--captions-path', type=str, required=True,
+                    help='path to unfiltered captions')
+parser.add_argument('-s', '--save-path', type=str, required=True,
+                    help='path to save filtered captions')
+args = parser.parse_args()
+
+captions_path = args.captions_path
+save_path = args.save_path
 
 ids = os.listdir('saved_features')
 filtered_captions = {}
 
-with open('raw_caption_superclean.json', 'r') as input_file:
+with open(captions_path, 'r') as input_file:
     captions_json = ijson.kvitems(input_file, '')
     for vid_id, captions in tqdm(captions_json):
         if vid_id in ids:
             filtered_captions[vid_id] = captions
 
-json.dump(filtered_captions, open('filtered_captions.json', 'w'))
+json.dump(filtered_captions, open(save_path, 'w'))

7c1da4f99e12b9d8865197eba605c61e13027249
ammesatyajit <ammesatyajit@gmail.com>
2021-01-22 18:17:55 -0800

    modifying save_centroid_imgs.py

--- VideoBERT/data/save_centroid_imgs.py
+++ VideoBERT/data/save_centroid_imgs.py
@@ -1,6 +1,6 @@
 import os
 import json
-import tqdm
+from tqdm import tqdm
 import shutil
 
 centroid_img_file = 'centroid_to_img.json'
@@ -9,6 +9,5 @@ save_dir = 'centroid_imgs'
 os.mkdir(save_dir)
 centroid_map_json = json.load(open(centroid_img_file, 'r'))
 
-for key in centroid_map_json:
-    print(key, centroid_map_json[key])
+for key in tqdm(centroid_map_json):
     shutil.copy(centroid_map_json[key], os.path.join(save_dir, "centroid-{:05}".format(int(key))))

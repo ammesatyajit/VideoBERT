f634a671936f1e3d8db841b90e874bd13f53e804
Satyajit <ammesatyajit@gmail.com>
2021-02-15 18:40:16 -0800

    label_data.py more user-friendly

--- VideoBERT/data/label_data.py
+++ VideoBERT/data/label_data.py
@@ -3,15 +3,25 @@ from tqdm import tqdm
 from sklearn.cluster import MiniBatchKMeans
 import os
 import json
+import argparse
 
-features_root = 'saved_features'
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-f', '--root-features', type=str, required=True,
+                    help='path to folder containing all the video folders with the features')
+parser.add_argument('-c', '--centroid-file', type=str, required=True,
+                    help='the .npy file containing all the centroids')
+parser.add_argument('-s', '--save-file', type=str, required=True,
+                    help='json file to save the labelled data to')
+args = parser.parse_args()
+
+features_root = args.root_features
 dirs = os.listdir(features_root)
 
-centroids = np.load('centroids.npy')
 kmeans = MiniBatchKMeans()
-kmeans.cluster_centers_ = centroids
+kmeans.cluster_centers_ = np.load(args.centroid_file)
 
-save_path = 'labelled_data.json'
+save_path = args.save_file
 
 data_dict = {}
 
